9cc387d863eef9211bbae241979ba0c53d2f12c3
Srinu-7 <srinumaka9908@gmail.com>
2025-01-08 09:01:23 +0530

    upto full footer

--- src/Customer/Pages/HomePage/HomePage.jsx
+++ src/Customer/Pages/HomePage/HomePage.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import MainCarousel from '../../Component/HomeCarousel/MainCarousel';
 import HomeSectionCarousel from '../../Component/HomeSectionCarousel/HomeSectionCarousel';
 import { mens_kurta } from '../../../Data/menKurta';
+import { mensShoes } from '../../../Data/shoes';
+import { lengha_choli } from '../../../Data/LenghaCholi';
+import {saree} from '../../../Data/Saree';
+import {dress} from '../../../Data/Dress';
+import {gouns} from '../../../Data/Gouns';
+
+
 const HomePage = () => {
     return (
         <div>
             <MainCarousel />
             <div className = "space-y-10 py-10">
                 <HomeSectionCarousel data={mens_kurta} sectionName="Men's Kurta"/>
-                <HomeSectionCarousel data={mens_kurta} sectionName="Best Sellers"/>
+                <HomeSectionCarousel data={mens_kurta} sectionName="Men's Shoes"/>
                 <HomeSectionCarousel data={mens_kurta} sectionName="Trending"/>
                 <HomeSectionCarousel data={mens_kurta} sectionName="Top Rated"/>
                 <HomeSectionCarousel data={mens_kurta} sectionName="On Sale"/>
