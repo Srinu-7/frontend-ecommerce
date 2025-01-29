1f293dd1d1dad1f53401501bb8163e83f468dfa6
Srinu-7 <srinumaka9908@gmail.com>
2025-01-29 15:46:16 +0530

    changed base url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8081";
+export const API_BASE_URL = "http://ec2-13-60-167-22.eu-north-1.compute.amazonaws.com:8081";
 
 const jwt = localStorage.getItem("jwt");
 
